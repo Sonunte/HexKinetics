d7eb4c83ac2f75f13932bcc811fe35a70f7748a9
Sonunte <sonunte@gmail.com>
2023-05-02 12:29:38 +0200

    Lesser Teleport Finally

--- Common/src/main/java/net/sonunte/hexforce/common/casting/actions/spells/OpLesserTeleport.kt
+++ Common/src/main/java/net/sonunte/hexforce/common/casting/actions/spells/OpLesserTeleport.kt
@@ -5,14 +5,9 @@ import at.petrak.hexcasting.api.spell.*
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
 import at.petrak.hexcasting.common.casting.operators.spells.great.OpTeleport
-import at.petrak.hexcasting.xplat.IXplatAbstractions
-import net.minecraft.core.BlockPos
 import net.minecraft.util.Mth
 import net.minecraft.world.entity.Entity
-import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.phys.Vec3
-import net.sonunte.hexforce.api.config.HexForceConfig
-import net.sonunte.hexforce.common.casting.actions.OpExampleConstMediaAction.argc
 import kotlin.math.floor
 
 object OpLesserTeleport : SpellAction {
@@ -24,7 +19,7 @@ object OpLesserTeleport : SpellAction {
 		val entity = args.getEntity(0, argc)
 		val number = args.getDouble(1, argc)
 		val position = entity.position()
-		val fraction = Mth.clamp(Math.abs(number), 0.0, 99.99999999)
+		val fraction = Mth.clamp(Math.abs(number), 0.0001, 99.99999999)
 		ctx.assertEntityInRange(entity)
 
 		return Triple(
@@ -38,9 +33,27 @@ object OpLesserTeleport : SpellAction {
 		override fun cast(ctx: CastingContext) {
 			val pos = entity.position()
 
-			//entity.setPos(Vec3(floor(pos.x), floor(pos.y), floor(pos.z)))
-			OpTeleport.teleportRespectSticky(entity, Vec3(floor(pos.x), floor(pos.y), floor(pos.z)), ctx.world)
-			OpTeleport.teleportRespectSticky(entity, Vec3((floor(pos.x) + fraction / 100) - pos.x, (floor(pos.y) + fraction / 100) - pos.y, (floor(pos.z) + fraction / 100) - pos.z),  ctx.world)
+			OpTeleport.teleportRespectSticky(entity, Vec3(floor(pos.x) - pos.x, 0.0, floor(pos.z) - pos.z), ctx.world)
+
+
+			//OpTeleport.teleportRespectSticky(entity, Vec3((floor(pos.x) + fraction / 100) - floor(pos.x), (floor(pos.y) + fraction / 100) - floor(pos.y), (floor(pos.z) + fraction / 100) - floor(pos.z)),  ctx.world)
+
+
+			if (pos.x < 0){
+				OpTeleport.teleportRespectSticky(entity, Vec3((floor(pos.x) + (1.0 - fraction / 100)) - floor(pos.x), 0.0, 0.0), ctx.world)
+			} else{
+				OpTeleport.teleportRespectSticky(entity, Vec3((floor(pos.x) + fraction / 100) - floor(pos.x), 0.0, 0.0), ctx.world)
+			}
+			if (pos.y < 0){
+				OpTeleport.teleportRespectSticky(entity, Vec3(0.0, (floor(pos.y) + (1.0 - fraction / 100)) - floor(pos.y), 0.0), ctx.world)
+			} else{
+				OpTeleport.teleportRespectSticky(entity, Vec3(0.0, (floor(pos.y) + fraction / 100) - floor(pos.y), 0.0), ctx.world)
+			}
+			if (pos.z < 0){
+				OpTeleport.teleportRespectSticky(entity, Vec3(0.0, 0.0, (floor(pos.z) + (1.0 - fraction / 100)) - floor(pos.z)), ctx.world)
+			} else{
+				OpTeleport.teleportRespectSticky(entity, Vec3(0.0, 0.0, (floor(pos.z) + fraction / 100) - floor(pos.z)), ctx.world)
+			}
 
 		}
 	}
