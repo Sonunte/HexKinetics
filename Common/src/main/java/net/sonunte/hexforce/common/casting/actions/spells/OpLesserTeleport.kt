c13fe40380d3004614caf480732ef73609269269
Sonunte <sonunte@gmail.com>
2023-05-02 10:58:37 +0200

    Lesser Teleport Almost Works

--- Common/src/main/java/net/sonunte/hexforce/common/casting/actions/spells/OpLesserTeleport.kt
+++ Common/src/main/java/net/sonunte/hexforce/common/casting/actions/spells/OpLesserTeleport.kt
@@ -4,8 +4,10 @@ import at.petrak.hexcasting.api.misc.MediaConstants
 import at.petrak.hexcasting.api.spell.*
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.common.casting.operators.spells.great.OpTeleport
 import at.petrak.hexcasting.xplat.IXplatAbstractions
 import net.minecraft.core.BlockPos
+import net.minecraft.util.Mth
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.phys.Vec3
@@ -19,9 +21,10 @@ object OpLesserTeleport : SpellAction {
 	private const val COST = (MediaConstants.DUST_UNIT * 0.5).toInt()
 
 	override fun execute(args: List<Iota>, ctx: CastingContext): Triple<RenderedSpell, Int, List<ParticleSpray>> {
-		val entity = args.getEntity(0, OpLesserTeleport.argc)
-		val fraction = args.getDouble(1, argc)
+		val entity = args.getEntity(0, argc)
+		val number = args.getDouble(1, argc)
 		val position = entity.position()
+		val fraction = Mth.clamp(Math.abs(number), 0.0, 99.99999999)
 		ctx.assertEntityInRange(entity)
 
 		return Triple(
@@ -36,7 +39,8 @@ object OpLesserTeleport : SpellAction {
 			val pos = entity.position()
 
 			//entity.setPos(Vec3(floor(pos.x), floor(pos.y), floor(pos.z)))
-			entity.setPos(Vec3(pos.x + fraction.toBigDecimal().toDouble(), pos.y + fraction.toBigDecimal().toDouble(), pos.z + fraction.toBigDecimal().toDouble()))
+			OpTeleport.teleportRespectSticky(entity, Vec3(floor(pos.x), floor(pos.y), floor(pos.z)), ctx.world)
+			OpTeleport.teleportRespectSticky(entity, Vec3((floor(pos.x) + fraction / 100) - pos.x, (floor(pos.y) + fraction / 100) - pos.y, (floor(pos.z) + fraction / 100) - pos.z),  ctx.world)
 
 		}
 	}
