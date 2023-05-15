16130d22c156b8d3b5662f2086e594266287063f
Sonunte <sonunte@gmail.com>
2023-05-15 20:19:09 +0200

    Add Gravity cost implemented

--- Common/src/main/java/net/sonunte/hexkinetics/common/casting/actions/spells/OpAddGravity.kt
+++ Common/src/main/java/net/sonunte/hexkinetics/common/casting/actions/spells/OpAddGravity.kt
@@ -5,32 +5,30 @@ import at.petrak.hexcasting.api.misc.MediaConstants
 import at.petrak.hexcasting.api.spell.*
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
-import net.minecraft.nbt.CompoundTag
-import net.minecraft.nbt.IntTag
 import net.minecraft.world.entity.Entity
-import net.sonunte.hexkinetics.common.casting.actions.great_spells.OpGreaterImpulse
+import kotlin.math.pow
 
 
 object OpAddGravity : SpellAction {
 
-	override val argc = 1
-	private var uses = 1
+	override val argc = 2
 
 	override fun execute(args: List<Iota>, ctx: CastingContext): Triple<RenderedSpell, Int, List<ParticleSpray>> {
 		val entity = args.getEntity(0, argc)
+		val blocks = args.getDouble(1, argc)
 		ctx.assertEntityInRange(entity)
 
-		val cost = MediaConstants.DUST_UNIT * 5  * uses
+		val cost = (MediaConstants.DUST_UNIT * 5 * blocks * (entity.fallDistance / 5)).toInt()
 
 		return Triple(
-			Spell(entity),
+			Spell(entity, blocks),
 			cost,
 			listOf(ParticleSpray.burst(entity.position(), 1.0))
 		)
 	}
-	private data class Spell(val entity: Entity) : RenderedSpell {
+	private data class Spell(val entity: Entity, val blocks: Double) : RenderedSpell {
 		override fun cast(ctx: CastingContext) {
-			entity.fallDistance = entity.fallDistance + 1
+			entity.fallDistance = entity.fallDistance + blocks.toFloat()
 		}
 	}
 }
