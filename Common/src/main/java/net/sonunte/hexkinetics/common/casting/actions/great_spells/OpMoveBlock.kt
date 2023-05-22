2186071490495638b769d477f8aeecd6b61c7722
Sonunte <sonunte@gmail.com>
2023-05-22 19:49:19 +0200

    Some basic code for Translocation

--- Common/src/main/java/net/sonunte/hexkinetics/common/casting/actions/great_spells/OpMoveBlock.kt
+++ Common/src/main/java/net/sonunte/hexkinetics/common/casting/actions/great_spells/OpMoveBlock.kt
@@ -1,13 +1,18 @@
 package net.sonunte.hexkinetics.common.casting.actions.great_spells
 
 import at.petrak.hexcasting.api.misc.MediaConstants
-import at.petrak.hexcasting.api.spell.*
+import at.petrak.hexcasting.api.spell.ParticleSpray
+import at.petrak.hexcasting.api.spell.RenderedSpell
+import at.petrak.hexcasting.api.spell.SpellAction
 import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.getVec3
 import at.petrak.hexcasting.api.spell.iota.Iota
-import net.minecraft.util.Mth
-import net.minecraft.world.entity.Entity
-import net.minecraft.world.entity.projectile.Projectile
+import net.minecraft.core.BlockPos
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.phys.Vec3
+import org.apache.logging.log4j.LogManager
 
 object OpMoveBlock : SpellAction {
 
@@ -29,7 +34,40 @@ object OpMoveBlock : SpellAction {
 	private data class Spell(val block: Vec3, val offset: Vec3) : RenderedSpell {
         override fun cast(ctx: CastingContext) {
 
+			val destination = block.add(offset)
+			val blockPosDestination = convertToBlockPos(destination)
+			val blockPos = convertToBlockPos(block)
+
+			if (!isAir(blockPosDestination, ctx)) { null } //block is not air, exit early
+			else {
+
+				placeBlock(ctx.world, blockPos, blockPosDestination)
+
+			}
+
         }
 	}
+
+	fun convertToBlockPos(vec: Vec3): BlockPos {
+		return BlockPos(vec.x.toInt(), vec.y.toInt(), vec.z.toInt())
+	}
+
+	fun isAir(blockPos: BlockPos, ctx: CastingContext): Boolean {
+		val world = ctx.world
+		val blockState = world.getBlockState(blockPos)
+		val block = blockState.block
+
+		return block == Blocks.AIR
+	}
+	fun placeBlock(world: ServerLevel, pos: BlockPos, destination: BlockPos, nbtData: CompoundTag? = null) {
+		val blockState = world.getBlockState(pos)
+
+		if (nbtData != null) {
+			LogManager.getLogger().debug("Has nbtData")
+		}
+
+		world.setBlockAndUpdate(destination, blockState)
+	}
+
 
 }
